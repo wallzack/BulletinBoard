5c73ae40fe5d561f499769ab2ebc4710c06017ee
wallzack <vulcheek@gmail.com>
2020-05-02 13:48:10 +0200

    26.3 aded db.js file

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -5,41 +5,23 @@ import CardGroup from 'react-bootstrap/CardGroup';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-import { connect } from 'react-redux';
-import { getUser } from '../../../redux/userRedux.js';
-
 import clsx from 'clsx';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './Homepage.module.scss';
-const myads = [
-  {
-    id: 1,
-    title: 'TIDAL Premium 1 year',
-    location: 'Łódź',
-    publishedDate: 'today',
-    image: 'https://tidal.com/_nuxt/img/620975c.png',
-  },
-  {
-    id: 2,
-    title: 'Check new Spotify',
-    location: null,
-    publishedDate: 'today',
-    image: 'https://static.wirtualnemedia.pl/media/top/Spotify_nowyekran655.png',
-  },
-];
-const Component = ({ className, user }) => (
-  <div className={clsx(className, styles.root)}>
+import { myads } from '../../../db';
 
+const Component = ({ className }) => (
+  <div className={clsx(className, styles.root)}>
     <h3>My ads</h3>
     <CardGroup>
       {myads.map(ad => (
-        <Card className={styles.ad} key={ad.id}>
+        <Card {...ad} className={styles.ad} key={ad.id}>
           <Card.Img className={styles.cardImage} variant="top" src={ad.image} />
           <Card.Body>
-            <Card.Title><a href={`/post/edit/:${ad.id}`}>{ad.title}</a></Card.Title>
+            <Card.Title><a href={`/post/:${ad.id}`}>{ad.title}</a></Card.Title>
             <Card.Text>
               {ad.location}
             </Card.Text>
@@ -50,29 +32,26 @@ const Component = ({ className, user }) => (
         </Card>
       ))}
     </CardGroup>
-    {user.authenticated ? (
-      <Button href="/post/add" variant="dark">Add new post</Button>
-    ) : ''}
+    <Button href="/post/add" variant="dark">Add new post</Button>
   </div>
 );
 
 Component.propTypes = {
   className: PropTypes.string,
-  user: PropTypes.object,
 };
 
-const mapStateToProps = state => ({
-  user: getUser(state),
-});
+// const mapStateToProps = state => ({
+//   someProp: reduxSelector(state),
+// });
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-const Container = connect(mapStateToProps)(Component);
+// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  // Component as Homepage,
-  Container as Homepage,
+  Component as Homepage,
+  // Container as Homepage,
   Component as HomepageComponent,
 };
