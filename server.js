ce7bb4d88e55fcbc350d78f3d021f57a54e95032
Walczyk Emil <Emil.Walczyk@coi.gov.pl>
2020-05-10 18:11:54 +0200

    27.2 init backend final

--- server.js
+++ server.js
@@ -1,41 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const mongoose = require('mongoose');
-
-const postsRoutes = require('./routes/posts.routes');
+const helmet = require('helmet');
 
 const app = express();
-
-/* MIDDLEWARE */
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-/* API ENDPOINTS */
-app.use('/api', postsRoutes);
+app.use(express.static(path.join(__dirname + '/client/src')));
 
-/* API ERROR PAGES */
-app.use('/api', (req, res) => {
-  res.status(404).send({ post: 'Not found...' });
-});
-
-/* REACT WEBSITE */
-app.use(express.static(path.join(__dirname, '../build')));
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-});
-
-/* MONGOOSE */
-mongoose.connect('mongodb://localhost:27017/bulletinBoard', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.once('open', () => {
-  console.log('Successfully connected to the database');
-});
-db.on('error', err => console.log('Error: ' + err));
-
-/* START SERVER */
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log('Server is running on port: '+port);
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
